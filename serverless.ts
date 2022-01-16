c029fb0a58547caa41fd8df57e75e714c6c5c614
yassun <yassun.engineer.tips@gmail.com>
2022-01-16 17:56:12 +0900

    東京リージョンにデプロイする

--- serverless.ts
+++ serverless.ts
@@ -9,6 +9,7 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: 'ap-northeast-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
