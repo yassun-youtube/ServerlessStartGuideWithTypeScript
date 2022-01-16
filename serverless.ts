60f8a2d3506f5a45040d031623cd68aa1b4e3619
yassun <yassun.engineer.tips@gmail.com>
2022-01-16 17:17:27 +0900

    serverless offline を追加

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,7 @@ import hello from '@functions/hello';
 const serverlessConfiguration: AWS = {
   service: 'serverless-test-app',
   frameworkVersion: '2',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
